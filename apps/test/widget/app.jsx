941782fe609cf5561367b180d94e860270b4115d
aliveevie <aliveibi080@gmail.com>
2024-03-26 17:14:59 +0100

    Initial Commit

---
+++ apps/test/widget/app.jsx
@@ -0,0 +1,14 @@
+import { Editor } from '@monaco-editor/react';
+
+
+return (
+    <div>
+    <Editor 
+      height="70vh"
+      width={800}
+      defaultLanguage="typescript" 
+      theme='vs-dark'
+    />
+
+    </div>
+)
